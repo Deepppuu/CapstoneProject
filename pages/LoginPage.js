de86f10faaa3d4bf4b9fcf06aacfadb3fc010a4c
Deepppuu <160409637+Deepppuu@users.noreply.github.com>
2026-04-04 21:54:40 +0530

    Fix login page URL and enhance navigation
    Updated the login page URL and added waitUntil option.

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -9,14 +9,22 @@ this.loginBtn = 'button:has-text("Login")';
 }
 
 async openLoginPage(){
-await this.page.goto('http://127.0.0.1:5500/Capstone-Frontend/login.html');
+
+await this.page.goto(
+'http://127.0.0.1:5500/login.html',
+{ waitUntil: 'domcontentloaded' }
+);
+
 await this.page.waitForSelector(this.email);
+
 }
 
 async login(email,password){
+
 await this.page.fill(this.email,email);
 await this.page.fill(this.password,password);
 await this.page.click(this.loginBtn);
+
 }
 
 }
