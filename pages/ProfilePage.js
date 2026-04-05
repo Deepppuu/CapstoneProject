152e1a54a07f3c82818af9ac41d54c9dc04c5566
Deepppuu <160409637+Deepppuu@users.noreply.github.com>
2026-04-05 12:27:44 +0530

    Refactor ProfilePage class with new locators and methods

--- pages/ProfilePage.js
+++ pages/ProfilePage.js
@@ -3,63 +3,85 @@ import { expect } from '@playwright/test';
 export class ProfilePage {
 
 constructor(page){
+
 this.page = page;
 
-/* LOCATORS */
-this.title = page.locator("h4.profile-title");
+this.profileCard = page.locator(".profile-card");
+this.navbar = page.locator("#navbar");
+
 this.userId = page.locator("#userId");
 this.name = page.locator("#name");
 this.email = page.locator("#email");
+
 this.profileInitial = page.locator("#profileInitial");
 
 this.logoutBtn = page.locator(".logout-btn");
-this.navbar = page.locator("#navbar");
+
 }
 
-/* NAVIGATION */
+/* NAVIGATE */
 
 async navigate(){
+
 await this.page.goto(
-  "http://127.0.0.1:5500/Capstone-Frontend/profile.html",
-  { waitUntil: "domcontentloaded" }
+"http://127.0.0.1:5500/profile.html",
+{ waitUntil: "domcontentloaded" }
 );
+
 }
 
-/* VALIDATIONS */
+/* PAGE VALIDATIONS */
 
 async verifyPageLoaded(){
-await expect(this.title).toHaveText("My Profile");
+
+await expect(this.profileCard).toBeVisible();
+
 }
 
 async verifyNavbarLoaded(){
+
 await expect(this.navbar).toBeVisible();
+
 }
 
 async verifyUserId(){
+
 await expect(this.userId).not.toBeEmpty();
+
 }
 
 async verifyName(){
+
 await expect(this.name).not.toBeEmpty();
+
 }
 
 async verifyEmail(){
+
 await expect(this.email).not.toBeEmpty();
+
 }
 
 async verifyProfileInitial(){
+
 const text = await this.profileInitial.textContent();
-expect(text.length).toBe(1);
+
+expect(text.length).toBeGreaterThan(0);
+
 }
 
 async verifyLogoutVisible(){
+
 await expect(this.logoutBtn).toBeVisible();
+
 }
 
 /* ACTIONS */
 
 async clickLogout(){
+
 await this.logoutBtn.click();
+
 }
 
 }
